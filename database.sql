53ff0ae57492eb40a25934efe3d6f15f0117ea64
Yassine El hassani <yassine.ds.404@gmail.com>
2025-01-16 08:50:46 +0100

    SQL querys that I used to creat the database

---
+++ database.sql
@@ -0,0 +1,64 @@
+-- creation of the database
+CREATE DATABASE Youdemy;
+
+-- Users table
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('admin', 'teacher', 'student') NOT NULL,
+    status ENUM('active', 'suspended', 'pending') NOT NULL
+);
+
+-- Categories table
+CREATE TABLE categories (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL
+);
+
+-- Tags table
+CREATE TABLE tags (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL
+);
+
+-- Courses table
+CREATE TABLE courses (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    content TEXT NOT NULL,
+    user_id INT NOT NULL,
+    category_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
+);
+
+-- Course_tags table
+CREATE TABLE course_tags (
+    course_id INT NOT NULL,
+    tag_id INT NOT NULL,
+    PRIMARY KEY (course_id, tag_id),
+    FOREIGN KEY (course_id) REFERENCES courses(id),
+    FOREIGN KEY (tag_id) REFERENCES tags(id)
+);
+
+-- Create trigger
+DELIMITER $$
+
+CREATE TRIGGER set_default_status 
+BEFORE INSERT ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.role = 'teacher' THEN
+        SET NEW.status = 'pending';
+    ELSE
+        SET NEW.status = 'active';
+    END IF;
+END $$
+
+DELIMITER ;
+
+
+
