29b2b9ceb0f3260b23507f035b2ee3ad2647f5e7
Yassine El hassani <yassine.ds.404@gmail.com>
2025-01-18 19:32:54 +0100

    Making the separation of layouts

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,185 +1,107 @@
-<?php 
+<?php
 
 session_start();
 
 if (!isset($_SESSION["id"]) || $_SESSION["role"] !== "admin") {
-  header("Location: ../login/index.php");
-  exit();
+    header("Location: ../login/index.php");
+    exit();
 }
 
 
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../src/css/dashboard.css">
-    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <title>Admin Dashboard</title>
-</head>
-<body>
-    <nav>
-        <div class="logo-name">
-            <div class="logo-image">
-                <img src="../src/img/logo.jpg" alt="">
-            </div>
+<?php
+include_once './layout/header.php';
+?>
 
-            <span class="logo_name">Youdemy</span>
+<div class="dash-content">
+    <div class="overview">
+        <div class="title">
+            <i class="uil uil-tachometer-fast-alt"></i>
+            <span class="text">Dashboard</span>
         </div>
 
-        <div class="menu-items">
-            <ul class="nav-links">
-                <li><a href="./index.php">
-                    <i class="uil uil-estate"></i>
-                    <span class="link-name">Dashboard</span>
-                </a></li>
-                <li><a href="./statistics.php">
-                    <i class="uil uil-chart"></i>
-                    <span class="link-name">Statistics</span>
-                </a></li>
-                <li><a href="./manageUsers.php">
-                    <i class="uil uil-users-alt"></i>
-                    <span class="link-name">Manage Users</span>
-                </a></li>
-                <li><a href="./manageCourses.php">
-                    <i class="uil uil-files-landscapes"></i>
-                    <span class="link-name">Manage Courses</span>
-                </a></li>
-                <li><a href="./manageCategories.php">
-                    <i class="uil uil-create-dashboard"></i>
-                    <span class="link-name">Manage Categories</span>
-                </a></li>
-                <li><a href="./manageTags.php">
-                    <i class="uil uil-tag"></i>
-                    <span class="link-name">Manage Tags</span>
-                </a></li>
-            </ul>
-            
-            <ul class="logout-mode">
-                <li><a href="../logout.php">
-                    <i class="uil uil-signout"></i>
-                    <span class="link-name">Logout</span>
-                </a></li>
-
-                <li class="mode">
-                    <a href="#">
-                        <i class="uil uil-moon"></i>
-                        <span class="link-name">Dark Mode</span>
-                    </a>
-
-                    <div class="mode-toggle">
-                        <span class="switch"></span>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    </nav>
-
-    <section class="dashboard">
-        <div class="top">
-            <i class="uil uil-bars sidebar-toggle"></i>
-
-            <div class="search-box">
-                <i class="uil uil-search"></i>
-                <input type="text" placeholder="Search here...">
+        <div class="boxes">
+            <div class="box box1">
+                <i class="uil uil-thumbs-up"></i>
+                <span class="text">Total Likes</span>
+                <span class="number">50,120</span>
             </div>
-            
-            <img src="../src/img/profile.php" alt="">
-        </div>
-
-        <div class="dash-content">
-            <div class="overview">
-                <div class="title">
-                    <i class="uil uil-tachometer-fast-alt"></i>
-                    <span class="text">Dashboard</span>
-                </div>
-
-                <div class="boxes">
-                    <div class="box box1">
-                        <i class="uil uil-thumbs-up"></i>
-                        <span class="text">Total Likes</span>
-                        <span class="number">50,120</span>
-                    </div>
-                    <div class="box box2">
-                        <i class="uil uil-comments"></i>
-                        <span class="text">Comments</span>
-                        <span class="number">20,120</span>
-                    </div>
-                    <div class="box box3">
-                        <i class="uil uil-share"></i>
-                        <span class="text">Total Share</span>
-                        <span class="number">10,120</span>
-                    </div>
-                </div>
+            <div class="box box2">
+                <i class="uil uil-comments"></i>
+                <span class="text">Comments</span>
+                <span class="number">20,120</span>
             </div>
-
-            <div class="activity">
-                <div class="title">
-                    <i class="uil uil-clock-three"></i>
-                    <span class="text">Recent Activity</span>
-                </div>
-
-                <div class="activity-data">
-                    <div class="data names">
-                        <span class="data-title">Name</span>
-                        <span class="data-list">Prem Shahi</span>
-                        <span class="data-list">Deepa Chand</span>
-                        <span class="data-list">Manisha Chand</span>
-                        <span class="data-list">Pratima Shahi</span>
-                        <span class="data-list">Man Shahi</span>
-                        <span class="data-list">Ganesh Chand</span>
-                        <span class="data-list">Bikash Chand</span>
-                    </div>
-                    <div class="data email">
-                        <span class="data-title">Email</span>
-                        <span class="data-list">premshahi@gmail.com</span>
-                        <span class="data-list">deepachand@gmail.com</span>
-                        <span class="data-list">prakashhai@gmail.com</span>
-                        <span class="data-list">manishachand@gmail.com</span>
-                        <span class="data-list">pratimashhai@gmail.com</span>
-                        <span class="data-list">manshahi@gmail.com</span>
-                        <span class="data-list">ganeshchand@gmail.com</span>
-                    </div>
-                    <div class="data joined">
-                        <span class="data-title">Joined</span>
-                        <span class="data-list">2022-02-12</span>
-                        <span class="data-list">2022-02-12</span>
-                        <span class="data-list">2022-02-13</span>
-                        <span class="data-list">2022-02-13</span>
-                        <span class="data-list">2022-02-14</span>
-                        <span class="data-list">2022-02-14</span>
-                        <span class="data-list">2022-02-15</span>
-                    </div>
-                    <div class="data type">
-                        <span class="data-title">Type</span>
-                        <span class="data-list">New</span>
-                        <span class="data-list">Member</span>
-                        <span class="data-list">Member</span>
-                        <span class="data-list">New</span>
-                        <span class="data-list">Member</span>
-                        <span class="data-list">New</span>
-                        <span class="data-list">Member</span>
-                    </div>
-                    <div class="data status">
-                        <span class="data-title">Status</span>
-                        <span class="data-list">Liked</span>
-                        <span class="data-list">Liked</span>
-                        <span class="data-list">Liked</span>
-                        <span class="data-list">Liked</span>
-                        <span class="data-list">Liked</span>
-                        <span class="data-list">Liked</span>
-                        <span class="data-list">Liked</span>
-                    </div>
-                </div>
+            <div class="box box3">
+                <i class="uil uil-share"></i>
+                <span class="text">Total Share</span>
+                <span class="number">10,120</span>
             </div>
         </div>
-    </section>
+    </div>
 
-    <script src="../src/js/dashboard.js"></script>
-</body>
-</html>
+    <div class="activity">
+        <div class="title">
+            <i class="uil uil-clock-three"></i>
+            <span class="text">Recent Activity</span>
+        </div>
+
+        <div class="activity-data">
+            <div class="data names">
+                <span class="data-title">Name</span>
+                <span class="data-list">Prem Shahi</span>
+                <span class="data-list">Deepa Chand</span>
+                <span class="data-list">Manisha Chand</span>
+                <span class="data-list">Pratima Shahi</span>
+                <span class="data-list">Man Shahi</span>
+                <span class="data-list">Ganesh Chand</span>
+                <span class="data-list">Bikash Chand</span>
+            </div>
+            <div class="data email">
+                <span class="data-title">Email</span>
+                <span class="data-list">premshahi@gmail.com</span>
+                <span class="data-list">deepachand@gmail.com</span>
+                <span class="data-list">prakashhai@gmail.com</span>
+                <span class="data-list">manishachand@gmail.com</span>
+                <span class="data-list">pratimashhai@gmail.com</span>
+                <span class="data-list">manshahi@gmail.com</span>
+                <span class="data-list">ganeshchand@gmail.com</span>
+            </div>
+            <div class="data joined">
+                <span class="data-title">Joined</span>
+                <span class="data-list">2022-02-12</span>
+                <span class="data-list">2022-02-12</span>
+                <span class="data-list">2022-02-13</span>
+                <span class="data-list">2022-02-13</span>
+                <span class="data-list">2022-02-14</span>
+                <span class="data-list">2022-02-14</span>
+                <span class="data-list">2022-02-15</span>
+            </div>
+            <div class="data type">
+                <span class="data-title">Type</span>
+                <span class="data-list">New</span>
+                <span class="data-list">Member</span>
+                <span class="data-list">Member</span>
+                <span class="data-list">New</span>
+                <span class="data-list">Member</span>
+                <span class="data-list">New</span>
+                <span class="data-list">Member</span>
+            </div>
+            <div class="data status">
+                <span class="data-title">Status</span>
+                <span class="data-list">Liked</span>
+                <span class="data-list">Liked</span>
+                <span class="data-list">Liked</span>
+                <span class="data-list">Liked</span>
+                <span class="data-list">Liked</span>
+                <span class="data-list">Liked</span>
+                <span class="data-list">Liked</span>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+include_once './layout/footer.php';
+?>
