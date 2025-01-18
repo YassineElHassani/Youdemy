c7b2ebba0ce6d9bcfc38960c4cfee148b1c00827
Yassine El hassani <yassine.ds.404@gmail.com>
2025-01-18 19:36:02 +0100

    Remaking the structer of activation

--- dashboard/userActivate.php
+++ dashboard/userActivate.php
@@ -1,10 +1,10 @@
 <?php
-require_once '../config/connection.php';
+require_once '../class/Admin.php';
+
+$activate = new Admin();
 
 if(isset($_GET["id"])) {
-    $conn = Database::getConnection();
-    $stmt = $conn->prepare("UPDATE users SET status = 'active' WHERE id = $_GET[id]");
-    $stmt->execute();
+    $activate->activateUser();
 }
 
 header('Location: ./manageUsers.php');
