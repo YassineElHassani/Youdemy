1905135e25768fc55e3c32ac99085d100fd69099
Yassine El hassani <yassine.ds.404@gmail.com>
2025-01-20 22:43:24 +0100

    Sub operation page

---
+++ subCourse.php
@@ -0,0 +1,23 @@
+<?php
+require_once './config/connection.php';
+require_once './class/CoursesManager.php';
+
+session_start();
+
+if (!isset($_SESSION['id'])) {
+    header("Location: login/index.php");
+    exit();
+}
+
+$userId = $_SESSION['id'];
+
+
+if(isset($_GET['id'])) {
+    $coursesManager = new CoursesManager();
+    $message = $coursesManager->subscribeToCourse($userId, $_GET['id']);
+}
+
+header("Location: home.php");
+exit();
+
+?>
