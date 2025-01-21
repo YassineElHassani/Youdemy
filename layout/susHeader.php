2718112a9b654020e80471f1c37552f44bcc6691
Yassine El hassani <yassine.ds.404@gmail.com>
2025-01-21 23:32:35 +0100

    Adding header for suspanding page

---
+++ layout/susHeader.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Account Suspended</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="./src/css/homeStyle.css">
+    <link rel="shortcut icon" href="./src/img/ico.png" type="image/x-icon">
+</head>
+
+<body class="bg-[#535353]">
+    <header>
+        <nav class="navbar">
+            <span class="hamburger-btn material-symbols-rounded">menu</span>
+            <a href="/" class="logo">
+                <img src="./src/img/logo.png" alt="logo">
+                <h2>Youdemy</h2>
+            </a>
+            <ul class="links">
+                <li><a href="#">Courses</a></li>
+                <li><a href="#">About us</a></li>
+                <li><a href="#">Contact us</a></li>
+            </ul>
+        </nav>
+    </header>
