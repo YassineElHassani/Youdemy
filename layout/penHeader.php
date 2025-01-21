699fb06c189e471691795920435b7297171c03cb
Yassine El hassani <yassine.ds.404@gmail.com>
2025-01-21 23:32:15 +0100

    Adding header for pending page

---
+++ layout/penHeader.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Account Pending Activation</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="./src/css/homeStyle.css">
+    <link rel="shortcut icon" href="./src/img/ico.png" type="image/x-icon">
+</head>
+
+<body class="bg-[#535353]">
+    <header>
+        <nav class="navbar">
+            <span class="hamburger-btn material-symbols-rounded">menu</span>
+            <a href="#" class="logo">
+                <img src="./src/img/logo.png" alt="logo">
+                <h2>Youdemy</h2>
+            </a>
+            <ul class="links">
+                <li><a href="#">Courses</a></li>
+                <li><a href="#">About us</a></li>
+                <li><a href="#">Contact us</a></li>
+            </ul>
+            <div style="display: flex; align-items: center; gap: 10px;">
+                <a href="./logout.php"><img src="./src/img/logout.png" height="25px" width="25px" alt="Logout"></a>
+            </div>
+        </nav>
+    </header>
