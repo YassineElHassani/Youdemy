46c84df9946db4c72b669d12f3b05dc94986bc6c
Yassine El hassani <yassine.ds.404@gmail.com>
2025-01-21 23:41:58 +0100

    Adding the suspending page

---
+++ suspending.php
@@ -0,0 +1,18 @@
+<?php 
+include_once './layout/susHeader.php';
+?>
+
+<div class="bg-white p-8 shadow-lg max-w-full text-center mt-[150px]">
+    <img src="./src/img/ban.png" alt="Pending Approval" class="w-24 h-24 mx-auto mb-6">
+    <h1 class="text-2xl font-bold text-gray-800 mb-4">Your Account Has Been Suspended</h1>
+    <p class="text-gray-600 mb-6">
+        You don't have the access to logIn to your account.
+    </p>
+    <p class="text-gray-600 mb-6">
+        Your account is suspended. contact support if you have any questions.
+    </p>
+</div>
+
+<?php
+include_once './layout/stateFooter.php';
+?>
