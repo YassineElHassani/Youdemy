70b692057a5962e45d414c7f6f68b5b0cccd3fff
Yassine El hassani <yassine.ds.404@gmail.com>
2025-01-21 23:30:29 +0100

    Adding chart statistic for users

--- dashboard/statistics.php
+++ dashboard/statistics.php
@@ -1,4 +1,5 @@
 <?php
+require_once '../class/Admin.php';
 
 session_start();
 
@@ -7,6 +8,10 @@ if (!isset($_SESSION["id"]) || $_SESSION["role"] !== "admin") {
     exit();
 }
 
+$stats = new Admin();
+$totalActiveUsers = $stats->totalActiveUsers();
+$totalDeactivatedUsers = $stats->totalDeactivatedUsers();
+$totalSuspendedUser = $stats->totalSuspendedUser();
 
 ?>
 
@@ -23,9 +28,39 @@ include_once './layout/header.php';
     </div>
 
     <div class="activity">
+        <div class="activity-data" style="padding-left: 20%;">
+            <div style="height: 800px; width: 750px; border-radius: 30px; padding: 10px;">
+                <canvas id="chart" style="margin-top: 20px;"></canvas>
+            </div>
+            <script>
+                const totalActiveUsers = <?php echo htmlspecialchars($totalActiveUsers); ?>;
+                const totalDeactivatedUsers = <?php echo htmlspecialchars($totalDeactivatedUsers); ?>;
+                const totalSuspendedUser = <?php echo htmlspecialchars($totalSuspendedUser); ?>;
 
-        <div class="activity-data">
+                const data = {
+                    labels: ['Active Users', 'Deactivated Users', 'Suspended User'],
+                    datasets: [{
+                        label: 'Dataset Overview',
+                        data: [totalActiveUsers, totalDeactivatedUsers, totalSuspendedUser],
+                        backgroundColor: [
+                            'rgb(75, 222, 151)',
+                            'rgb(255, 173, 50)',
+                            'rgb(255, 88, 88)'
+                        ]
+                    }]
+                };
 
+                const config = {
+                    type: 'polarArea',
+                    data: data,
+                    options: {
+                        responsive: true,
+                    }
+                };
+
+                const ctx = document.getElementById('chart').getContext('2d');
+                new Chart(ctx, config);
+            </script>
         </div>
     </div>
 </div>
