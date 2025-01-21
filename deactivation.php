862b2c7f33a76926755dae7cc2d536e35c0155e0
Yassine El hassani <yassine.ds.404@gmail.com>
2025-01-21 23:39:17 +0100

    Adding deactivating page

---
+++ deactivation.php
@@ -0,0 +1,18 @@
+<?php 
+include_once './layout/deaHeader.php';
+?>
+
+<div class="bg-white p-8 shadow-lg max-w-full text-center mt-[150px]">
+    <img src="./src/img/deactivated.png" alt="Pending Approval" class="w-24 h-24 mx-auto mb-6">
+    <h1 class="text-2xl font-bold text-gray-800 mb-4">Your Account Has Been Deactivated</h1>
+    <p class="text-gray-600 mb-6">
+        You don't have the access to logIn to your account.
+    </p>
+    <p class="text-gray-600 mb-6">
+        Please contact us to reactivate your account or if you have any questions.
+    </p>
+</div>
+
+<?php
+include_once './layout/stateFooter.php';
+?>
