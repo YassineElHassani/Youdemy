fd4623632c4c4fb41769e29b434096cab0ca8bc0
Yassine El hassani <yassine.ds.404@gmail.com>
2025-01-22 15:34:39 +0100

    Changing the class for operation function

--- subCourse.php
+++ subCourse.php
@@ -1,6 +1,6 @@
 <?php
 require_once './config/connection.php';
-require_once './class/CoursesManager.php';
+require_once './class/Student.php';
 
 session_start();
 
@@ -13,8 +13,8 @@ $userId = $_SESSION['id'];
 
 
 if(isset($_GET['id'])) {
-    $coursesManager = new CoursesManager();
-    $message = $coursesManager->subscribeToCourse($userId, $_GET['id']);
+    $course = new Student();
+    $message = $course->subscribeToCourse($userId, $_GET['id']);
 }
 
 header("Location: myCourses.php");
