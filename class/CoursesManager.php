d0bb268f54e34139d3a779d1f41af8b45f58fdcd
Yassine El hassani <yassine.ds.404@gmail.com>
2025-01-22 15:37:42 +0100

    Rmove the functions of subs

--- class/CoursesManager.php
+++ class/CoursesManager.php
@@ -153,39 +153,6 @@ class CoursesManager {
         return true;
     }
 
-    public function getCoursesBySubs($userId) {
-        $conn = Database::getConnection();
-        $stmt = $conn->prepare("SELECT courses.id AS course_id, courses.image AS course_image, courses.title AS course_title, courses.description AS course_description
-            FROM subscription INNER JOIN courses ON subscription.course_id = courses.id
-            WHERE subscription.user_id = :user_id;
-        ");
-        $stmt->execute([':user_id' => $userId]);
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
-
-    public function subscribeToCourse($userId, $id) {
-        $conn = Database::getConnection();
-    
-        $stmt = $conn->prepare("SELECT COUNT(*) FROM subscription WHERE user_id = :user_id AND course_id = :course_id");
-        $stmt->execute([
-            ':user_id' => $userId,
-            ':course_id' => $id
-        ]);
-        $isSubscribed = $stmt->fetchColumn();
-    
-        if ($isSubscribed) {
-            return ['success' => false];
-        }
-    
-        $stmt = $conn->prepare("INSERT INTO subscription (user_id, course_id) VALUES (:user_id, :course_id)");
-        $stmt->execute([
-            ':user_id' => $userId,
-            ':course_id' => $id
-        ]);
-    
-        return ['success' => true];
-    }
-
     public function getCoursesWithPagination($limit, $offset) {
         $conn = Database::getConnection();
         $stmt = $conn->prepare("SELECT COUNT(*) AS total FROM courses");
@@ -205,7 +172,5 @@ class CoursesManager {
             'totalCourses' => $totalCourses
         ];
     }
-    
-    
     
 }
