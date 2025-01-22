2176dd8760001601c9f2392da7482ef1bdedb6ca
Yassine El hassani <yassine.ds.404@gmail.com>
2025-01-22 13:36:29 +0100

    Adding satitstics

--- platform/statistics.php
+++ platform/statistics.php
@@ -1,4 +1,5 @@
 <?php
+require_once '../class/Teacher.php';
 
 session_start();
 
@@ -7,6 +8,11 @@ if (!isset($_SESSION["id"]) || $_SESSION["role"] !== "teacher") {
     exit();
 }
 
+$userId = $_SESSION['id'];
+
+$stats = new Teacher();
+$totalCourses = $stats->totalCourses($userId);
+$totalSubscribers = $stats->totalSubscribers($userId);
 
 ?>
 
@@ -20,13 +26,28 @@ include_once './layout/header.php';
             <i class="uil uil-tachometer-fast-alt"></i>
             <span class="text">Statistics</span>
         </div>
-    </div>
 
-    <div class="activity">
-
-        <div class="activity-data">
-
+        <div class="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-6">
+            <div class="bg-blue-500 text-white p-6 rounded-lg shadow-lg flex items-center justify-between">
+                <div>
+                    <img src="../src/img/course.png" height="40px" width="40px">
+                </div>
+                <div>
+                    <p class="text-lg font-semibold">Total Courses</p>
+                    <p class="text-2xl font-bold"><?php echo htmlspecialchars($totalCourses); ?></p>
+                </div>
+            </div>
+            <div class="bg-green-500 text-white p-6 rounded-lg shadow-lg flex items-center justify-between">
+                <div>
+                    <img src="../src/img/student.png" height="40px" width="40px">
+                </div>
+                <div>
+                    <p class="text-lg font-semibold">Total Subscribers</p>
+                    <p class="text-2xl font-bold"><?php echo htmlspecialchars($totalSubscribers) ?></p>
+                </div>
+            </div>
         </div>
+
     </div>
 </div>
 
